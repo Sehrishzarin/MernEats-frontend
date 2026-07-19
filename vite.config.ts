3881c0fcf7b62b73eabae4e6f16cb4934c49f586
Sehrish Zarin <83836984+Sehrishzarin@users.noreply.github.com>
2026-07-19 11:48:39 +0500

    Configure base path for Vite
    Set base path for Azure deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This tells Vite to use relative paths so Azure can find your compiled JS/CSS files
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
